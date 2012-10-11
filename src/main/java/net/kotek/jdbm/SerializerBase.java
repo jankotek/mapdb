34434e588f2d76080dd89c3a756a866315065d17
Jan Kotek <jan@kotek.net>
2012-10-11 23:54:08 +0100

    Fix small bug in serializer (credit Geoff Hendrey)

--- src/main/java/net/kotek/jdbm/SerializerBase.java
+++ src/main/java/net/kotek/jdbm/SerializerBase.java
@@ -193,7 +193,7 @@ public class SerializerBase implements Serializer{
             else if (val == 1)
                 out.write(BYTE_1);
             else {
-                out.write(SHORT_FULL);
+                out.write(BYTE_FULL);
                 out.writeByte(val);
             }
             return;
