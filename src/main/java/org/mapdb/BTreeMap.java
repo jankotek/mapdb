c3649bf6d6ba3e02ba884cbb9af51dee44e924cf
Jan Kotek <jan@kotek.net>
2012-11-16 23:12:33 +0000

    Add License

--- src/main/java/org/mapdb/BTreeMap.java
+++ src/main/java/org/mapdb/BTreeMap.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright (c) 2012 Jan Kotek
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
 /*
  * NOTE: some code (and javadoc) used in this class
@@ -7,7 +22,6 @@
  * Expert Group and released to the public domain, as explained at
  * http://creativecommons.org/licenses/publicdomain
  */
-
 
 package org.mapdb;
 
@@ -57,6 +71,9 @@ import java.util.concurrent.ConcurrentNavigableMap;
  * null return values cannot be reliably distinguished from the absence of
  * elements.
  *
+ *
+ * @author Jan Kotek
+ * @author some parts by Doug Lea
  */
 @SuppressWarnings("unchecked")
 public class BTreeMap<K,V> extends AbstractMap<K,V>
