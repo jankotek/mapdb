faea4396308c5ed4e826ab6c93c549787c60917e
Jan Kotek <jan@kotek.net>
2012-11-28 00:54:02 +0000

    Yet another fix for concurrent instance cache

--- src/main/java/org/mapdb/CacheHashTable.java
+++ src/main/java/org/mapdb/CacheHashTable.java
@@ -16,12 +16,9 @@
 
 package org.mapdb;
 
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.concurrent.atomic.AtomicReferenceArray;
-
 /**
  * Fixed size cache which uses hash table.
- * Is thread-safe and does not require any locks.
+ * Is thread-safe and requires only minimal locking.
  * Items are randomly removed and replaced by hash collisions.
  * <p/>
  * This is simple, concurrent, small-overhead, random cache.
@@ -30,9 +27,14 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
  */
 public class CacheHashTable implements Engine {
 
+    private static final int CONCURRENCY_FACTOR = 16;
+
+    protected final Object[] locks;
+
+
     protected Engine engine;
 
-    protected AtomicReferenceArray<HashItem> items;
+    protected HashItem[] items;
     protected final int cacheMaxSize;
 
     private class HashItem {
@@ -49,15 +51,19 @@ public class CacheHashTable implements Engine {
 
     public CacheHashTable(Engine engine, int cacheMaxSize) {
         this.engine = engine;
-        this.items = new AtomicReferenceArray(cacheMaxSize);
+        this.items = new HashItem[cacheMaxSize];
         this.cacheMaxSize = cacheMaxSize;
+        this.locks = new Object[CONCURRENCY_FACTOR];
+        for(int i=0;i<locks.length;i++)
+            locks[i] = new Object();
+
     }
 
     @Override
     public <A> long recordPut(A value, Serializer<A> serializer) {
+        //no need for locking, as recid is not propagated outside of method yet
         final long recid = engine.recordPut(value, serializer);
-        final int pos = Math.abs(Utils.longHash(recid))%cacheMaxSize;
-        items.set(pos,new HashItem(recid, value));
+        items[Math.abs(Utils.longHash(recid))%cacheMaxSize] = new HashItem(recid, value);
         return recid;
     }
 
@@ -65,32 +71,37 @@ public class CacheHashTable implements Engine {
     @SuppressWarnings("unchecked")
     public <A> A recordGet(long recid, Serializer<A> serializer) {
         final int pos = Math.abs(Utils.longHash(recid))%cacheMaxSize;
-        HashItem item = items.get(pos);
+        HashItem item = items[pos];
         if(item!=null && recid == item.key)
             return (A) item.val;
 
-        //not in cache, fetch and add
-        final A value = engine.recordGet(recid, serializer);
-        if(value!=null)
-            items.compareAndSet(pos,item, new HashItem(recid, value));
-        return value;
+        synchronized (locks[pos % CONCURRENCY_FACTOR]){
+            //not in cache, fetch and add
+            final A value = engine.recordGet(recid, serializer);
+            if(value!=null)
+                items[pos] = new HashItem(recid, value);
+            return value;
+        }
     }
 
     @Override
     public <A> void recordUpdate(long recid, A value, Serializer<A> serializer) {
-        final int pos = Math.abs(Math.abs(Utils.longHash(recid)))%cacheMaxSize;
-        HashItem expected = items.get(pos);
-        engine.recordUpdate(recid, value, serializer);
-        items.compareAndSet(pos,expected, new HashItem(recid, value));
+        final int pos = Math.abs(Utils.longHash(recid))%cacheMaxSize;
+        synchronized (locks[pos % CONCURRENCY_FACTOR]){
+            items[pos] = new HashItem(recid, value);
+            engine.recordUpdate(recid, value, serializer);
+        }
     }
 
     @Override
     public void recordDelete(long recid) {
         final int pos = Math.abs(Utils.longHash(recid))%cacheMaxSize;
-        HashItem expected = items.get(pos);
-        engine.recordDelete(recid);
-        if(expected!=null && recid == expected.key)
-            items.compareAndSet(pos,expected,null);
+        synchronized (locks[pos % CONCURRENCY_FACTOR]){
+            engine.recordDelete(recid);
+            HashItem item = items[pos];
+            if(item!=null && recid == item.key)
+            items[pos] = null;
+        }
     }
 
 
@@ -109,8 +120,8 @@ public class CacheHashTable implements Engine {
 
     @Override
     public void rollback() {
-        for(int i = 0;i<cacheMaxSize;i++)
-            items.set(i,null);
+        for(int i = 0;i<items.length;i++)
+            items[i] = null;
         engine.rollback();
     }
 
