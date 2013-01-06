4450ff6575ce6fefa924f3e99ff9a7fd235bf040
Jan Kotek <jan@kotek.net>
2013-01-06 22:53:09 +0000

    Fixed Issue #42, HTreeMap created with wrong settings.

--- src/main/java/org/mapdb/DB.java
+++ src/main/java/org/mapdb/DB.java
@@ -154,7 +154,7 @@ public class DB {
     @NotNull
     synchronized public <K> Set<K> createHashSet(String name, Serializer<K> serializer){
         checkNameNotExists(name);
-        HTreeMap<K,Object> ret = new HTreeMap<K,Object>(engine, true, defaultSerializer, serializer, null);
+        HTreeMap<K,Object> ret = new HTreeMap<K,Object>(engine, false, defaultSerializer, serializer, null);
         nameDir.put(name, ret.rootRecid);
         Set<K> ret2 = ret.keySet();
         collections.put(name, new WeakReference<Object>(ret2));
@@ -269,7 +269,7 @@ public class DB {
     @NotNull
     synchronized public <K> NavigableSet<K> createTreeSet(String name, int nodeSize, Serializer<K[]> serializer, Comparator<K> comparator){
         checkNameNotExists(name);
-        BTreeMap<K,Object> ret = new BTreeMap<K,Object>(engine, nodeSize, true, false, defaultSerializer, serializer, null, comparator);
+        BTreeMap<K,Object> ret = new BTreeMap<K,Object>(engine, nodeSize, false, false, defaultSerializer, serializer, null, comparator);
         nameDir.put(name, ret.treeRecid);
         NavigableSet<K> ret2 = ret.keySet();
         collections.put(name, new WeakReference<Object>(ret2));
