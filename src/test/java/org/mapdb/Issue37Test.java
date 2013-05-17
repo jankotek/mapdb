86bbcc73a81ac3f5de52f26d795fafa05146ff38
Jan Kotek <jan@kotek.net>
2013-05-17 01:01:30 +0100

    Remove redundant testcase from Issue #37

--- src/test/java/org/mapdb/Issue37Test.java
+++ src/test/java/org/mapdb/Issue37Test.java
@@ -18,37 +18,6 @@ import static org.junit.Assert.fail;
 public class Issue37Test {
 
 
-    @Test
-    public void test2() throws IOException{
-
-        /**
-         * Tijdelijke on-disk database gebruiken.
-         */
-        DB db = DBMaker.newTempFileDB().cacheDisable().make();
-        ConcurrentMap<String,String> testData = db.createHashMap("test",false,
-                null, null);
-
-        BufferedReader in = new BufferedReader(new FileReader("./src/test/resources/Issue37Data.txt"));
-        String line = null;
-
-        while ((line = in.readLine()) != null){
-            testData.put(line,line);
-        }
-
-        db.commit();
-
-        int printCount = 0;
-
-        for(String key : testData.keySet()){
-            printCount++;
-
-            String data = testData.get(key);
-
-            if(printCount > 10000){
-                fail();
-            }
-        }
-    }
 
     @Test public void test3(){
 
