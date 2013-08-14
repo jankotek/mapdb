d7216d68cb16fb551939d7dfed4e3c30cb63288a
Jan Kotek <jan@kotek.net>
2013-08-14 19:12:16 +0100

    StoreWAL: fix some TOMBSTONE details

--- src/main/java/org/mapdb/StoreWAL.java
+++ src/main/java/org/mapdb/StoreWAL.java
@@ -27,7 +27,7 @@ public class StoreWAL extends StoreDirect {
 
     public static final String TRANS_LOG_FILE_EXT = ".t";
 
-    protected static final long[] TOMBSTONE = new long[1];
+    protected static final long[] TOMBSTONE = new long[0];
 
     protected final Volume.Factory volFac;
     protected Volume log;
@@ -195,9 +195,9 @@ public class StoreWAL extends StoreDirect {
     protected <A> A get2(long ioRecid, Serializer<A> serializer) throws IOException {
         //check if record was modified in current transaction
         long[] r = modified.get(ioRecid);
-        //yes, read version
+        //no, read main version
         if(r==null) return super.get2(ioRecid, serializer);
-        //chech for tombstone (was deleted in current trans)
+        //check for tombstone (was deleted in current trans)
         if(r==TOMBSTONE || r.length==0) return null;
 
         //was modified in current transaction, so read it from trans log
@@ -692,7 +692,7 @@ public class StoreWAL extends StoreDirect {
 
     protected long[] getLinkedRecordsFromLog(long ioRecid){
         long[] ret0 = modified.get(ioRecid);
-        if(ret0!=null){
+        if(ret0!=null && ret0!=TOMBSTONE){
             long[] ret = new long[ret0.length];
             for(int i=0;i<ret0.length;i++){
                 long offset = ret0[i] & LOG_MASK_OFFSET;
