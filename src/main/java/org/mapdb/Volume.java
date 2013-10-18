dec6fe7298a00652aeb718f930fbc500d5fe2bb6
Jan Kotek <jan@kotek.net>
2013-10-18 16:41:37 +0300

    Volume: fix previous commit

--- src/main/java/org/mapdb/Volume.java
+++ src/main/java/org/mapdb/Volume.java
@@ -123,7 +123,7 @@ public abstract class Volume {
      * Writes a long to the indicated position
      */
     public void putSixLong(long pos, long value) {
-        assert(value>0 && (value>>>6*8)==0): "value does not fit";
+        assert(value>=0 && (value>>>6*8)==0): "value does not fit";
         //TODO read/write as integer+short, might be faster
         putByte(pos + 0, (byte) (0xff & (value >> 40)));
         putByte(pos + 1, (byte) (0xff & (value >> 32)));
@@ -643,7 +643,7 @@ public abstract class Volume {
 
         @Override
         public final void putSixLong(long offset, long value) {
-            assert(value>0 && (value>>>6*8)==0): "value does not fit";
+            assert(value>=0 && (value>>>6*8)==0): "value does not fit";
 
             try{
 
