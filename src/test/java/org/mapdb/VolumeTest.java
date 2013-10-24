180b22b598da2176d60e8a5f8928968d86fc9991
Jan Kotek <jan@kotek.net>
2013-10-24 14:18:09 +0300

    VolumeTest: Fix OOEM in some cases

--- src/test/java/org/mapdb/VolumeTest.java
+++ src/test/java/org/mapdb/VolumeTest.java
@@ -24,12 +24,12 @@ public abstract class VolumeTest {
 
         @Test public void transfer(){
             long max = (long) (Volume.BUF_SIZE*1.5);
-            Volume from = new Volume.MemoryVol(false,0);
+            Volume from = new Volume.MemoryVol(true,0);
             for(long i=0;i<max;i+=8){
                 from.ensureAvailable(i+8);
                 from.putLong(i,i);
             }
-            Volume to = new Volume.MemoryVol(false,0);
+            Volume to = new Volume.MemoryVol(true,0);
             Volume.volumeTransfer(max,from,to);
             for(long i=0;i<max;i+=8){
                 assertEquals(i, to.getLong(i));
