fcca003ecb74a414c42632eb0ed84433733ebf82
Jan Kotek <jan@kotek.net>
2014-07-15 15:42:50 +0300

    StoreWAL: replay on reopen could fail if log was corrupted. See #346
    Conflicts:
    	src/main/java/org/mapdb/StoreWAL.java
    	src/test/java/org/mapdb/BrokenDBTest.java

--- src/test/java/org/mapdb/StoreWALTest.java
+++ src/test/java/org/mapdb/StoreWALTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOError;
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.Assert.*;
 
@@ -80,5 +82,62 @@ public class StoreWALTest extends StoreDirectTest<StoreWAL>{
             }
             assertTrue(e2.getMessage().contains("version"));
         }
+    }
+
+    @Test public void discard_corrupted_log() throws IOException {
+
+        final AtomicBoolean replay = new AtomicBoolean(true);
+
+        StoreWAL wal = new StoreWAL(fac){
+            @Override
+            protected void replayLogFile() {
+                if(replay.get())
+                    super.replayLogFile();
+                else throw new IllegalAccessError();
+            }
+        };
+
+        DB db = new DB(wal);
+
+        Map m = db.getHashMap("map");
+
+        //fill map and commit
+        int max = (int) 1e5;
+        for(int i=0;i<max;i++){
+            m.put(i,i);
+        }
+        wal.commit();
+
+        //fill log, commit but do not replay
+        replay.set(false);
+        try {
+            for (int i = max; i < max*2; i++) {
+                m.put(i, i);
+            }
+            wal.commit();
+            fail("Should throw an error");
+        }catch(IllegalAccessError e){
+        }
+
+        //corrupt log randomly
+        wal.log.putLong(1000,111111111L);
+        wal.log.putLong(2000,111111111L);
+        wal.log.sync();
+        wal.log.close();
+        wal.phys.close();
+        wal.index.close();
+
+        //now reopen and check content
+        wal = new StoreWAL(fac);
+
+        db = new DB(wal);
+
+        m = db.getHashMap("map");
+
+        assertEquals(max, m.size());
+        for(int i=0;i<max;i++){
+            assertEquals(i,m.get(i));
+        }
+
     }
 }
