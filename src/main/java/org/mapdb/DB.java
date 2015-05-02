881e31a4ba43c3a95aaf04e8e2191d19d2f87f31
Jan Kotek <jan@kotek.net>
2015-05-02 23:41:35 +0300

    DB: Possible deadlock between DB.getNameForObject and DB.commit. Fix #501

--- src/main/java/org/mapdb/DB.java
+++ src/main/java/org/mapdb/DB.java
@@ -55,8 +55,7 @@ public class DB implements Closeable {
     protected Map<String, WeakReference<?>> namesInstanciated = new HashMap<String, WeakReference<?>>();
 
     protected Map<IdentityWrapper, String> namesLookup =
-            Collections.synchronizedMap( //TODO remove synchronized map, after DB locking is resolved
-            new HashMap<IdentityWrapper, String>());
+            new ConcurrentHashMap<IdentityWrapper, String>();
 
     /** view over named records */
     protected SortedMap<String, Object> catalog;
@@ -73,6 +72,7 @@ public class DB implements Closeable {
     //TODO collection get/create should be under consistencyLock.readLock()
     protected final ReadWriteLock consistencyLock;
 
+    /** changes object hash and equals method to use identity */
     protected static class IdentityWrapper{
 
         final Object o;
@@ -228,7 +228,7 @@ public class DB implements Closeable {
     }
 
     /** returns name for this object, if it has name and was instanciated by this DB*/
-    public synchronized  String getNameForObject(Object obj) {
+    public String getNameForObject(Object obj) {
         return namesLookup.get(new IdentityWrapper(obj));
     }
 
