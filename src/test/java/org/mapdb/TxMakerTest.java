424749f1e0fd21e8706b5fac213fe87d69980e30
Jan Kotek <jan@kotek.net>
2015-06-19 23:34:35 +0300

    TXMaker add some tests

--- src/test/java/org/mapdb/TxMakerTest.java
+++ src/test/java/org/mapdb/TxMakerTest.java
@@ -68,8 +68,8 @@ public class TxMakerTest{
         Exec.execNTimes(threads, new Callable() {
             @Override
             public Object call() throws Exception {
-                final int t=ii.incrementAndGet()*items*10000;
-                for (int index = t; index < t+items; index++) {
+                final int t = ii.incrementAndGet() * items * 10000;
+                for (int index = t; index < t + items; index++) {
                     final int temp = index;
                     s.add(temp);
                     tx.execute(new TxBlock() {
@@ -79,9 +79,9 @@ public class TxMakerTest{
 //							Queue<String> queue = db.getQueue(index + "");
 //							queue.offer(temp + "");
                             Map map = db.hashMap("ha");
-                            if(temp!=t)
-                                assertEquals(temp-1,map.get(temp-1));
-                            map.put(temp, temp );
+                            if (temp != t)
+                                assertEquals(temp - 1, map.get(temp - 1));
+                            map.put(temp, temp);
                         }
                     });
                 }
@@ -166,7 +166,7 @@ public class TxMakerTest{
         if(!ex.isEmpty())
             throw ex.get(0);
 
-        assertEquals(Long.valueOf(threads*items+1), tx.makeTx().getEngine().get(recid,Serializer.LONG));
+        assertEquals(Long.valueOf(threads * items + 1), tx.makeTx().getEngine().get(recid, Serializer.LONG));
 
 
     }
@@ -211,7 +211,7 @@ public class TxMakerTest{
         if(!ex.isEmpty())
             throw ex.get(0);
 
-        assertEquals(Long.valueOf(threads*items+1), tx.makeTx().getEngine().get(recid,Serializer.LONG));
+        assertEquals(Long.valueOf(threads * items + 1), tx.makeTx().getEngine().get(recid, Serializer.LONG));
 
     }
 
@@ -224,9 +224,9 @@ public class TxMakerTest{
                 .makeTxMaker();
 
         DB db = txMaker.makeTx();
-        long recid = db.getEngine().put("aa",Serializer.STRING);
+        long recid = db.getEngine().put("aa", Serializer.STRING);
         DB snapshot = db.snapshot();
-        db.getEngine().update(recid,"bb",Serializer.STRING);
+        db.getEngine().update(recid, "bb", Serializer.STRING);
         assertEquals("aa",snapshot.getEngine().get(recid,Serializer.STRING));
         assertEquals("bb",db.getEngine().get(recid,Serializer.STRING));
 
@@ -241,13 +241,115 @@ public class TxMakerTest{
                 .makeTxMaker();
 
         DB db = txMaker.makeTx();
-        long recid = db.getEngine().put("aa",Serializer.STRING);
+        long recid = db.getEngine().put("aa", Serializer.STRING);
         db.commit();
         db = txMaker.makeTx();
         DB snapshot = db.snapshot();
-        db.getEngine().update(recid,"bb",Serializer.STRING);
+        db.getEngine().update(recid, "bb", Serializer.STRING);
         assertEquals("aa",snapshot.getEngine().get(recid,Serializer.STRING));
         assertEquals("bb",db.getEngine().get(recid,Serializer.STRING));
 
     }
+
+
+    @Test @Ignore //TODO reenable test
+    public void testMVCC() {
+        TxMaker txMaker =
+                DBMaker.memoryDB().makeTxMaker();
+        {
+// set up the initial state of the database
+            DB tx = txMaker.makeTx();
+            BTreeMap<Object, Object> map = tx.createTreeMap("MyMap").valuesOutsideNodesEnable().make();
+            map.put("Value1", 1234);
+            map.put("Value2", 1000);
+            tx.commit();
+        }
+
+// Transaction A: read-only; used to check isolation level
+        DB txA = txMaker.makeTx();
+        BTreeMap<Object, Object> mapTxA = txA.getTreeMap("MyMap");
+
+// Transaction B: will set Value1 to 47
+        DB txB = txMaker.makeTx();
+        BTreeMap<Object, Object> mapTxB = txB.getTreeMap("MyMap");
+
+// Transaction C: will set Value2 to 2000
+        DB txC = txMaker.makeTx();
+        BTreeMap<Object, Object> mapTxC = txC.getTreeMap("MyMap");
+
+// perform the work in C (while B is open)
+        mapTxC.put("Value2", 2000);
+        txC.commit();
+
+// make sure that isolation level of Transaction A is not violated
+        assertEquals(1234, mapTxA.get("Value1"));
+        assertEquals(1000, mapTxA.get("Value2"));
+
+// perform work in B (note that we change different keys than in C)
+        mapTxB.put("Value1", 47);
+        txB.commit();  // FAILS with TxRollbackException
+
+// make sure that isolation level of Transaction A is not violated
+        assertEquals(1234, mapTxA.get("Value1"));
+        assertEquals(1000, mapTxA.get("Value2"));
+
+// Transaction D: read-only; used to check that commits were successful
+        DB txD = txMaker.makeTx();
+        BTreeMap<Object, Object> mapTxD = txD.getTreeMap("MyMap");
+
+// ensure that D sees the results of B and C
+        assertEquals(47, mapTxD.get("Value1"));
+        assertEquals(2000, mapTxD.get("Value2"));
+    }
+
+    @Test
+    public void testMVCCHashMap() {
+        TxMaker txMaker =
+                DBMaker.memoryDB().makeTxMaker();
+        {
+// set up the initial state of the database
+            DB tx = txMaker.makeTx();
+            Map<Object, Object> map = tx.createHashMap("MyMap").make();
+            map.put("Value1", 1234);
+            map.put("Value2", 1000);
+            tx.commit();
+        }
+
+// Transaction A: read-only; used to check isolation level
+        DB txA = txMaker.makeTx();
+        Map<Object, Object> mapTxA = txA.hashMap("MyMap");
+
+// Transaction B: will set Value1 to 47
+        DB txB = txMaker.makeTx();
+        Map<Object, Object> mapTxB = txB.hashMap("MyMap");
+
+// Transaction C: will set Value2 to 2000
+        DB txC = txMaker.makeTx();
+        Map<Object, Object> mapTxC = txC.hashMap("MyMap");
+
+// perform the work in C (while B is open)
+        mapTxC.put("Value2", 2000);
+        txC.commit();
+
+// make sure that isolation level of Transaction A is not violated
+        assertEquals(1234, mapTxA.get("Value1"));
+        assertEquals(1000, mapTxA.get("Value2"));
+
+// perform work in B (note that we change different keys than in C)
+        mapTxB.put("Value1", 47);
+        txB.commit();  // FAILS with TxRollbackException
+
+// make sure that isolation level of Transaction A is not violated
+        assertEquals(1234, mapTxA.get("Value1"));
+        assertEquals(1000, mapTxA.get("Value2"));
+
+// Transaction D: read-only; used to check that commits were successful
+        DB txD = txMaker.makeTx();
+        Map<Object, Object> mapTxD = txD.hashMap("MyMap");
+
+// ensure that D sees the results of B and C
+        assertEquals(47, mapTxD.get("Value1"));
+        assertEquals(2000, mapTxD.get("Value2"));
+    }
+
 }
