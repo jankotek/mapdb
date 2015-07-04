d1a65fbb304cf77c3ffb53686f630679bbbeef1c
Jan Kotek <jan@kotek.net>
2015-07-04 08:53:17 +0200

    Serializer: check java serializer works. See #536

--- src/test/java/org/mapdb/SerializerTest.java
+++ src/test/java/org/mapdb/SerializerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 
@@ -30,13 +31,37 @@ public class SerializerTest {
         byte[] b = new byte[100];
         new Random().nextBytes(b);
         Serializer<byte[]> ser = new Serializer.CompressionWrapper(Serializer.BYTE_ARRAY);
-        assertArrayEquals(b, SerializerBaseTest.clone2(b,ser));
+        assertArrayEquals(b, SerializerBaseTest.clone2(b, ser));
 
         b = Arrays.copyOf(b, 10000);
-        assertArrayEquals(b, SerializerBaseTest.clone2(b,ser));
+        assertArrayEquals(b, SerializerBaseTest.clone2(b, ser));
 
         DataOutput2 out = new DataOutput2();
         ser.serialize(out,b);
-        assertTrue(out.pos<1000);
+        assertTrue(out.pos < 1000);
+    }
+
+    @Test public void java_serializer_issue536(){
+        Long l = 1111L;
+        assertEquals(l, SerializerBaseTest.clone2(l, Serializer.JAVA));
+    }
+
+
+    @Test public void java_serializer_issue536_with_engine(){
+        DB db = DBMaker.newMemoryDB().transactionDisable().cacheDisable().make();
+        Long l = 1111L;
+        long recid = db.engine.put(l,Serializer.JAVA);
+        assertEquals(l, db.engine.get(recid, Serializer.JAVA));
+    }
+
+
+    @Test public void java_serializer_issue536_with_map(){
+        DB db = DBMaker.newMemoryDB().transactionDisable().cacheDisable().make();
+        Map m = db.createHashMap("map")
+                .keySerializer(Serializer.JAVA)
+                .make();
+        Long l = 1111L;
+        m.put(l,l);
+        assertEquals(l, m.get(l));
     }
 }
