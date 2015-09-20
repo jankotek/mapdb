b92d35b6b944eb533e7b5f11c716048defe2eaaf
Jan Kotek <jan@kotek.net>
2015-09-20 14:29:03 +0300

    Update header test

--- src/test/java/org/mapdb/StoreDirect2Test.java
+++ src/test/java/org/mapdb/StoreDirect2Test.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mapdb.DataIO.parity4Get;
 import static org.mapdb.DataIO.parity4Set;
 
 public class StoreDirect2Test {
@@ -15,6 +16,8 @@ public class StoreDirect2Test {
         s.init();
 
         assertTrue(s.vol == s.headVol);
+
+        assertEquals(StoreDirect2.HEADER_SIZE, parity4Get(s.headVol.getLong(StoreDirect2.O_STORE_SIZE)));
 
         for(long offset=StoreDirect2.O_STACK_FREE_RECID; offset<StoreDirect2.HEADER_SIZE;offset+=8){
             assertEquals(parity4Set(0), s.headVol.getLong(offset));
