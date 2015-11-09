803c3dae1d6a65da38dd30a7596270e9770b23bd
Jan Kotek <jan@kotek.net>
2015-11-09 12:31:42 +0200

    Doc: fix #611

--- src/test/java/doc/htreemap_cache_space_limit2.java
+++ src/test/java/doc/htreemap_cache_space_limit2.java
@@ -10,7 +10,7 @@ public class htreemap_cache_space_limit2 {
     public static void main(String[] args) {
         DB db = DBMaker.memoryDB().make();
         //a
-        HTreeMap cache = db.createHashMap("cache")
+        HTreeMap cache = db.hashMapCreate("cache")
                 .expireStoreSize(128)
                 .makeOrGet();
         //z
--- src/test/java/doc/htreemap_cache_ttl_limit.java
+++ src/test/java/doc/htreemap_cache_ttl_limit.java
@@ -12,10 +12,11 @@ public class htreemap_cache_ttl_limit {
     public static void main(String[] args) {
         DB db = DBMaker.memoryDB().make();
         //a
-        // remove entries 1H after their last modification, or 10 minutes after last get()
+        // remove entries 1 after their last modification,
+        // or 10 minutes after last get()
         HTreeMap cache = db.hashMapCreate("cache")
-                .expireAfterAccess(1, TimeUnit.HOURS)
-                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .expireAfterWrite(1, TimeUnit.HOURS)
+                .expireAfterAccess(10, TimeUnit.MINUTES)
                 .makeOrGet();
         //z
     }
