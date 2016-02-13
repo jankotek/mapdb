61ae8a026ff318980a952eccbd4603f4e5a80095
Jan Kotek <jan@kotek.net>
2016-02-13 15:05:21 +0200

    Add test for #664, not going to fix BTreeMap race condition

---
+++ src/test/java/org/mapdb/Issue664Test.java
@@ -0,0 +1,43 @@
+package org.mapdb;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+
+public class Issue664Test {
+
+    public static void main(String[] args) {
+        for(int i =0;i<1000;i++) {
+            testing();
+        }
+    }
+
+    private static void testing() {
+        DBMaker m = DBMaker.newTempFileDB().deleteFilesAfterClose();
+        m = m.transactionDisable();
+        m = m.compressionEnable();
+        m = m.cacheDisable();
+        m = m.asyncWriteEnable();
+        m = m.closeOnJvmShutdown();
+        DB db = m.make();
+        Map<Integer,HashMap> tmp = db.createTreeMap("test")
+                        .counterEnable()
+                        .makeOrGet();
+
+        IntStream.rangeClosed(0, 499).parallel().forEach(i -> {
+            Object old = tmp.put(i, new HashMap<>());
+            //System.out.println("Old "+old);
+        });
+
+        int n =tmp.size();
+        //System.out.println(n);
+        if(n!=500) {
+            throw new RuntimeException("The numbers don't match");
+        }
+
+
+        db.close();
+    }
+}
