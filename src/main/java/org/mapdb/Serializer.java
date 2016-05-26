24f5b47e2c63778800410bdaa4c8a831bb44b72b
Per-Åke Minborg <minborg@speedment.com>
2016-05-25 20:53:09 -0700

    Document base serializers

--- src/main/java/org/mapdb/Serializer.java
+++ src/main/java/org/mapdb/Serializer.java
@@ -47,9 +47,9 @@ public interface Serializer<A /*extends Comparable<? super A>*/> extends Compara
 
     /**
      * A predefined {@link Serializer} that handles non-null
-     * {@link String Strings} whereby serialized Strings are serialized to a
-     * UTF-8 encoded format. The Serializer also stores the String's size,
-     * allowing it to be used as a collection serializer.
+     * {@link String Strings} whereby Strings are serialized to a UTF-8 encoded
+     * format. The Serializer also stores the String's size, allowing it to be
+     * used as a collection serializer.
      * <p>
      * This Serializer hashes Strings using the original
      * {@link String#hashCode()} method as opposed to the
@@ -64,9 +64,9 @@ public interface Serializer<A /*extends Comparable<? super A>*/> extends Compara
 
     /**
      * A predefined {@link Serializer} that handles non-null
-     * {@link String Strings} whereby serialized Strings are serialized to a
-     * UTF-8 encoded format. The Serializer also stores the String's size,
-     * allowing it to be used as a collection serializer.
+     * {@link String Strings} whereby Strings are serialized to a UTF-8 encoded
+     * format. The Serializer also stores the String's size, allowing it to be
+     * used as a collection serializer.
      * <p>
      * This Serializer hashes Strings using a specially tailored
      * {@link String#hashCode()} method as opposed to the
@@ -79,76 +79,208 @@ public interface Serializer<A /*extends Comparable<? super A>*/> extends Compara
      */
     GroupSerializer<String> STRING = new SerializerString();
 
+    /**
+     * A predefined {@link Serializer} that handles non-null
+     * {@link String Strings} whereby Strings are serialized to a UTF-8 encoded
+     * format. The Serializer also stores the String's size, allowing it to be
+     * used as a collection serializer. Neighboring strings may be delta encoded
+     * for increased storage efficency.
+     * <p>
+     * This Serializer hashes Strings using a specially tailored
+     * {@link String#hashCode()} method as opposed to the
+     * {@link Serializer#STRING_ORIGHASH} Serializer.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
+     * @see Serializer#STRING
+     */
     GroupSerializer<String> STRING_DELTA = new SerializerStringDelta();
+
+    /**
+     * A predefined {@link Serializer} that handles non-null
+     * {@link String Strings} whereby Strings are serialized to a UTF-8 encoded
+     * format. The Serializer also stores the String's size, allowing it to be
+     * used as a collection serializer. Neighboring strings may be delta encoded
+     * for increased storage efficency.
+     * <p>
+     * This Serializer hashes Strings using a specially tailored
+     * {@link String#hashCode()} method as opposed to the
+     * {@link Serializer#STRING_ORIGHASH} Serializer.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
+     * @see Serializer#STRING
+     */
     GroupSerializer<String> STRING_DELTA2 = new SerializerStringDelta2();
 
     /**
-     * Serializes strings using UTF8 encoding. Deserialized String is interned
-     * {@link String#intern()}, so it could save some memory.
+     * A predefined {@link Serializer} that handles non-null
+     * {@link String Strings} whereby Strings are serialized to a UTF-8 encoded
+     * format. The Serializer also stores the String's size, allowing it to be
+     * used as a collection serializer. Neighboring strings may be delta encoded
+     * for increased storage efficency.
+     * <p>
+     * Deserialized strings are automatically interned {@link String#intern()}
+     * allowing a more heap space efficient storage for repeated strings.
+     * <p>
+     * This Serializer hashes Strings using a specially tailored
+     * {@link String#hashCode()} method as opposed to the
+     * {@link Serializer#STRING_ORIGHASH} Serializer.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
      *
-     * Stores string size so can be used as collection serializer. Does not
-     * handle null values
+     * @see Serializer#STRING
      */
     GroupSerializer<String> STRING_INTERN = new SerializerStringIntern();
 
     /**
-     * Serializes strings using ASCII encoding (8 bit character). Is faster
-     * compared to UTF8 encoding. Stores string size so can be used as
-     * collection serializer. Does not handle null values
+     * A predefined {@link Serializer} that handles non-null
+     * {@link String Strings} whereby Strings are serialized to a ASCII encoded
+     * format (8 bit character) which is faster than using a UTF-8 format. The
+     * Serializer also stores the String's size, allowing it to be used as a
+     * collection serializer.
+     * <p>
+     * This Serializer hashes Strings using a specially tailored
+     * {@link String#hashCode()} method as opposed to the
+     * {@link Serializer#STRING_ORIGHASH} Serializer.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
+     * @see Serializer#STRING_ORIGHASH
      */
     GroupSerializer<String> STRING_ASCII = new SerializerStringAscii();
 
     /**
-     * Serializes strings using UTF8 encoding. Used mainly for testing. Does not
-     * handle null values.
+     * A predefined {@link Serializer} that handles non-null
+     * {@link String Strings} whereby Strings are serialized to a UTF-8 encoded
+     * format. The Serializer does <em>not</em> store the String's size, thereby
+     * preventing it from being used as a collection serializer.
+     * <p>
+     * This Serializer hashes Strings using the original
+     * {@link String#hashCode()} method as opposed to the
+     * {@link Serializer#STRING} Serializer.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
+     * @see Serializer#STRING_ORIGHASH
      */
     Serializer<String> STRING_NOSIZE = new SerializerStringNoSize();
 
     /**
-     * Serializes Long into 8 bytes, used mainly for testing. Does not handle
-     * null values.
+     * A predefined {@link Serializer} that handles non-null {@link Long Longs}
+     * whereby Longs are serialized to an 8 byte format.
+     * <p>
+     * This Serializer hashes Longs using the original {@link Long#hashCode()}
+     * method.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
      */
     GroupSerializer<Long> LONG = new SerializerLong();
 
     /**
-     * Packs positive LONG, so smaller positive values occupy less than 8 bytes.
-     * Large and negative values could occupy 8 or 9 bytes.
+     * A predefined {@link Serializer} that handles non-null {@link Long Longs}
+     * whereby Longs are serialized to a compressed byte format.
+     * <p>
+     * Smaller positive values occupy less than 8 bytes. Large and negative
+     * values could occupy 8 or 9 bytes.
+     * <p>
+     * This Serializer hashes Longs using the original {@link Long#hashCode()}
+     * method.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
      */
     GroupSerializer<Long> LONG_PACKED = new SerializerLongPacked();
 
     /**
-     * Applies delta packing on {@code java.lang.Long}. Difference between
-     * consequential numbers is also packed itself, so for small diffs it takes
-     * only single byte per number.
+     * A predefined {@link Serializer} that handles non-null {@link Long Longs}
+     * whereby Longs are serialized to a compressed byte format and neighboring
+     * Longs are delta encoded. Neighbors with a small delta can be encoded
+     * using a single byte.
+     * <p>
+     * Smaller positive values occupy less than 8 bytes. Large and negative
+     * values could occupy 8 or 9 bytes.
+     * <p>
+     * This Serializer hashes Longs using the original {@link Long#hashCode()}
+     * method.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
      */
     GroupSerializer<Long> LONG_DELTA = new SerializerLongDelta();
 
     /**
-     * Serializes Integer into 4 bytes, used mainly for testing. Does not handle
-     * null values.
+     * A predefined {@link Serializer} that handles non-null
+     * {@link Integer Integers} whereby Integers are serialized to a 4 byte
+     * format.
+     * <p>
+     * This Serializer hashes Integers using the original
+     * {@link Integer#hashCode()} method.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
      */
     GroupSerializer<Integer> INTEGER = new SerializerInteger();
 
     /**
-     * Packs positive Integer, so smaller positive values occupy less than 4
-     * bytes. Large and negative values could occupy 4 or 5 bytes.
+     * A predefined {@link Serializer} that handles non-null
+     * {@link Integer Integers} whereby Integers are serialized to a compressed
+     * byte format.
+     * <p>
+     * Smaller positive values occupy less than 4 bytes. Large and negative
+     * values could occupy 4 or 5 bytes.
+     * <p>
+     * This Serializer hashes Integers using the original
+     * {@link Integer#hashCode()} method.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
      */
     GroupSerializer<Integer> INTEGER_PACKED = new SerializerIntegerPacked();
 
     /**
-     * Applies delta packing on {@code java.lang.Integer}. Difference between
-     * consequential numbers is also packed itself, so for small diffs it takes
-     * only single byte per number.
+     * A predefined {@link Serializer} that handles non-null
+     * {@link Integer Integers} whereby Integers are serialized to a compressed
+     * byte format and neighboring Integers are delta encoded. Neighbors with a
+     * small delta can be encoded using a single byte.
+     * <p>
+     * Smaller positive values occupy less than 4 bytes. Large and negative
+     * values could occupy 4 or 5 bytes.
+     * <p>
+     * This Serializer hashes Integers using the original
+     * {@link Integer#hashCode()} method.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
      */
     GroupSerializer<Integer> INTEGER_DELTA = new SerializerIntegerDelta();
 
+    /**
+     * A predefined {@link Serializer} that handles non-null
+     * {@link Boolean Booleans} whereby Booleans are serialized to a one byte
+     * format.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
+     */
     GroupSerializer<Boolean> BOOLEAN = new SerializerBoolean();
 
-    ;
-
-
-    /** Packs recid + it adds 1bit checksum. */
-
+    /**
+     * Packs recid + it adds 1bit checksum.
+     */
     GroupSerializer<Long> RECID = new SerializerRecid();
 
     GroupSerializer<long[]> RECID_ARRAY = new SerializerRecidArray();
@@ -207,12 +339,57 @@ public interface Serializer<A /*extends Comparable<? super A>*/> extends Compara
      */
     GroupSerializer<java.util.UUID> UUID = new SerializerUUID();
 
+    /**
+     * A predefined {@link Serializer} that handles non-null {@link Byte Bytes}
+     * whereby Bytes are serialized to a one byte format.
+     * <p>
+     * This Serializer hashes Bytes using the original {@link Byte#hashCode()}
+     * method.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
+     */
     GroupSerializer<Byte> BYTE = new SerializerByte();
 
+    /**
+     * A predefined {@link Serializer} that handles non-null
+     * {@link Float Floats} whereby Floats are serialized to a 4 byte format.
+     * <p>
+     * This Serializer hashes Floats using the original {@link Float#hashCode()}
+     * method.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
+     */
     GroupSerializer<Float> FLOAT = new SerializerFloat();
 
+    /**
+     * A predefined {@link Serializer} that handles non-null
+     * {@link Double Doubles} whereby Doubles are serialized to an 8 byte
+     * format.
+     * <p>
+     * This Serializer hashes Doubles using the original
+     * {@link Double#hashCode()} method.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
+     */
     GroupSerializer<Double> DOUBLE = new SerializerDouble();
 
+    /**
+     * A predefined {@link Serializer} that handles non-null
+     * {@link Short Shorts} whereby Shorts are serialized to a 2 byte format.
+     * <p>
+     * This Serializer hashes Shorts using the original {@link Short#hashCode()}
+     * method.
+     * <p>
+     * If a {@code null} value is passed to the Serializer, a
+     * {@link NullPointerException} will be thrown.
+     *
+     */
     GroupSerializer<Short> SHORT = new SerializerShort();
 
 // TODO boolean array
@@ -286,7 +463,7 @@ public interface Serializer<A /*extends Comparable<? super A>*/> extends Compara
 //    };
 //
     /**
-     * Serialize the content of the given object into the given
+     * Serializes the content of the given value into the given
      * {@link DataOutput2}.
      *
      * @param out DataOutput2 to save object into
@@ -302,6 +479,7 @@ public interface Serializer<A /*extends Comparable<? super A>*/> extends Compara
      * @param input DataInput2 to de-serialize data from
      * @param available how many bytes that are available in the DataInput2 for
      * reading, may be -1 (in streams) or 0 (null).
+     *
      * @return the de-serialized content of the given {@link DataInput2}
      * @throws IOException in case of an I/O error
      */
@@ -384,7 +562,7 @@ public interface Serializer<A /*extends Comparable<? super A>*/> extends Compara
      * @param seed used to "scramble" the
      * @return a hash code of a non-null argument
      * @see Object#hashCode
-     * @throws NullPointerException if the
+     * @throws NullPointerException if the provided object is null
      */
     default int hashCode(@NotNull A o, int seed) {
         return DataIO.intHash(o.hashCode() + seed);
