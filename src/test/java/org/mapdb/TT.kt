360f446b1762491fc6722859c6f54ad21cc708d3
Jan Kotek <jan@kotek.net>
2017-02-04 21:36:25 +0200

    Add tests for #780

--- src/test/java/org/mapdb/TT.kt
+++ src/test/java/org/mapdb/TT.kt
@@ -155,7 +155,9 @@ object TT{
         return out.pos;
     }
 
-
+    fun <T : Any> forkJvm(clazz:Class<T>, vararg args : String): Process {
+        return JavaProcess.exec(clazz, args)
+    }
 
     fun fork(count:Int=1, body:(i:Int)->Unit){
         val finish = async(count=count, body=body)
