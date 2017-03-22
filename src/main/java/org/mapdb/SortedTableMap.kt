2c1a33cc50e3275127d7f3f857a873ce6399096e
Jan Kotek <jan@kotek.net>
2017-03-22 23:50:41 +0200

    SortedTableMap: add extra Page Size checks. Remove wrong check. Fix #815

--- src/main/java/org/mapdb/SortedTableMap.kt
+++ src/main/java/org/mapdb/SortedTableMap.kt
@@ -94,9 +94,12 @@ class SortedTableMap<K,V>(
                 valueSerializer: GroupSerializer<V>
         ): SortedTableMap<K, V> {
             val pageSize = volume.getLong(PAGE_SIZE_OFFSET)
-            if (pageSize <= 0 || pageSize > CC.PAGE_SIZE)
+            if (pageSize <= 0)
                 throw DBException.DataCorruption("Wrong page size: " + pageSize)
-            return SortedTableMap<K, V>(
+            val volSliceSize = volume.sliceSize()
+            if(volSliceSize>0 && volSliceSize<pageSize)
+                throw DBException.WrongConfiguration("Slice Size of underlying Volume is too small.")
+            return SortedTableMap(
                     keySerializer = keySerializer,
                     valueSerializer = valueSerializer,
                     volume = volume,
@@ -111,6 +114,9 @@ class SortedTableMap<K,V>(
                 pageSize: Long = CC.PAGE_SIZE,
                 nodeSize: Int = CC.BTREEMAP_MAX_NODE_SIZE
         ): Sink<K, V> {
+            val volSliceSize = volume.sliceSize()
+            if(volSliceSize>0 && volSliceSize<pageSize)
+                throw DBException.WrongConfiguration("Slice Size of underlying Volume is too small.")
 
             return object : Sink<K, V>() {
 
@@ -148,7 +154,7 @@ class SortedTableMap<K,V>(
                     volume.putLong(0L, CC.FILE_HEADER.shl(7 * 8) + CC.FILE_TYPE_SORTED_SINGLE.shl(6 * 8))
                     volume.putLong(SIZE_OFFSET, counter)
                     volume.putLong(PAGE_COUNT_OFFSET, (fileTail - pageSize) / pageSize)
-                    volume.putLong(PAGE_SIZE_OFFSET, pageSize.toLong())
+                    volume.putLong(PAGE_SIZE_OFFSET, pageSize)
                     volume.sync()
                     return SortedTableMap(
                             keySerializer = keySerializer,
@@ -262,7 +268,7 @@ class SortedTableMap<K,V>(
     /** first key at beginning of each page */
     protected val pageKeys = {
         val keys = ArrayList<K>()
-        for (i in 0..pageCount * pageSize step pageSize.toLong()) {
+        for (i in 0..pageCount * pageSize step pageSize) {
             val ii: Long = if (i == 0L) start.toLong() else i
             val offset = i + volume.getInt(ii + 4)
             val size = (i + volume.getInt(ii + 8) - offset).toInt()
---
+++ src/test/java/org/mapdb/TestWithTempDir.kt
@@ -0,0 +1,27 @@
+package org.mapdb
+
+import org.junit.After
+import java.io.File
+
+/**
+ * Test case with temporary dir,
+ * dir is deleted after tests finishes
+ */
+open abstract class TestWithTempDir{
+
+    private var tempDirCreated = false
+
+    fun tempFile() = File(tempDir, Math.random().toString())
+
+    val  tempDir: File by lazy{
+        tempDirCreated = true
+        TT.tempDir()
+    }
+
+    @After fun deleteTempDir(){
+        if(tempDirCreated)
+            TT.tempDeleteRecur(tempDir)
+    }
+
+
+}
