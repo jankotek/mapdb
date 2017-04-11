3fc81e1d32f072b8164ee70ca0903760deac5255
Jan Kotek <jan@kotek.net>
2017-04-11 17:49:15 +0300

    HTreeMap Concurrency Layout was not persisted after HTreeMap reopen. See #819

--- src/main/java/org/mapdb/DB.kt
+++ src/main/java/org/mapdb/DB.kt
@@ -866,6 +866,8 @@ open class DB(
         }
 
         override fun open2(catalog: SortedMap<String, String>): HTreeMap<K, V> {
+            _concShift = catalog[name + Keys.concShift]!!.toInt()
+
             val segmentCount = 1.shl(_concShift)
             val stores = Array(segmentCount, _storeFactory)
 
@@ -885,7 +887,6 @@ open class DB(
                     if ("" == counterRecidsStr) null
                     else counterRecidsStr.split(",").map { it.toLong() }.toLongArray()
 
-            _concShift = catalog[name + Keys.concShift]!!.toInt()
             _dirShift = catalog[name + Keys.dirShift]!!.toInt()
             _levels = catalog[name + Keys.levels]!!.toInt()
             _removeCollapsesIndexTree = catalog[name + Keys.removeCollapsesIndexTree]!!.toBoolean()
