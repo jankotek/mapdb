e40b537af2a909d4eaa6d2e24b4c1236f7c6c4ac
Jan Kotek <jan@kotek.net>
2018-06-18 20:16:38 +0200

    basic code generator

---
+++ buildSrc/src/main/java/MDBCodeGen.kt
@@ -0,0 +1,19 @@
+import org.gradle.internal.impldep.org.apache.commons.io.FileUtils
+import java.io.File
+
+class MDBCodeGen{
+        companion object {
+                @JvmStatic
+                fun main(args: Array<String>) {
+
+                        val srcDir = File("../srcGen/main/java")
+                        val testDir = File("../srcGen/test/java")
+
+
+                        FileUtils.write(File(srcDir, "aaGen.kt"),"""
+class AACodeGen{
+}
+                            """)
+                }
+        }
+}
---
+++ src/test/java/aaGenTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+
+class genTest{
+
+    @Test
+    fun main(){
+        println(AACodeGen())
+    }
+}
